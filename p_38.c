84896ad17187ccbfedf05038c7e7d6e51922f884
Nogueira Jr <nogueirajr@hotmail.com>
2021-08-26 08:08:55 -0300

    Create p_38.c

---
+++ p_38.c
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int convertBinaryToDecimal(long long);
+
+int main()
+{
+    long long n;
+
+    cout << "Enter a binary number: ";
+    cin >> n;
+ 
+    cout << n << " in binary = " << convertBinaryToDecimal(n) << "in decimal";
+    return 0;
+}
+
+int convertBinaryToDecimal(long long n)
+{
+    int decimalNumber = 0, i = 0, remainder;
+    while (n!=0)
+    {
+        remainder = n%10;
+        n /= 10;
+        decimalNumber += remainder*pow(2,i);
+        ++i;
+    }
+    return decimalNumber;
+}
+
+//Enter a binary number: 1111
+//1111 in binary = 15
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+long long convertDecimalToBinary(int);
+
+int main()
+{
+    int n, binaryNumber;
+
+    cout << "Enter a decimal number: ";
+    cin >> n;
+    binaryNumber = convertDecimalToBinary(n);
+    cout << n << " in decimal = " << binaryNumber << " in binary" << endl ;
+    return 0;
+}
+
+long long convertDecimalToBinary(int n)
+{
+    long long binaryNumber = 0;
+    int remainder, i = 1, step = 1;
+
+    while (n!=0)
+    {
+        remainder = n%2;
+        cout << "Step " << step++ << ": " << n << "/2, Remainder = " << remainder << ", Quotient = " << n/2 << endl;
+        n /= 2;
+        binaryNumber += remainder*i;
+        i *= 10;
+    }
+    return binaryNumber;
+}
+
+//Enter a decimal number: 19
+//Step 1: 19/2, Remainder = 1, Quotient = 9
+//Step 2: 9/2, Remainder = 1, Quotient = 4
+//Step 3: 4/2, Remainder = 0, Quotient = 2
+//Step 4: 2/2, Remainder = 0, Quotient = 1
+//Step 5: 1/2, Remainder = 1, Quotient = 0
+//19 in decimal = 10011 in binary
