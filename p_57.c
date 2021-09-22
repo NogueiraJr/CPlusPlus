3134e527832aafe3bc6353b7ac4bc8fca9d2dd0c
Nogueira Jr <nogueirajr@hotmail.com>
2021-09-22 13:31:25 -0300

    Create p_57.c

---
+++ p_57.c
@@ -0,0 +1,50 @@
+//Copy String Object
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s1, s2;
+
+    cout << "Enter string s1: ";
+    getline (cin, s1);
+
+    s2 = s1;
+
+    cout << "s1 = "<< s1 << endl;
+    cout << "s2 = "<< s2;
+
+    return 0;
+}
+
+//Enter string s1: C++ Strings
+//s1 = C++ Strings
+//s2 = C++ Strings
+
+//--------------------------------------------
+
+//Copy C-Strings
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    char s1[100], s2[100];
+
+    cout << "Enter string s1: ";
+    cin.getline(s1, 100);
+
+    strcpy(s2, s1);
+
+    cout << "s1 = "<< s1 << endl;
+    cout << "s2 = "<< s2;
+
+    return 0;
+}
+
+//Enter string s1: C-Strings
+//s1 = C-Strings
+//s2 = C-Strings
